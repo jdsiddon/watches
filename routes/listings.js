08fc68621f5dd099b10207bb6367b4390edea21d
justin <justin@delaris.com>
2016-04-24 01:37:44 -0700

    Listing to site connection working on view and create forms.

--- routes/listings.js
+++ routes/listings.js
@@ -3,24 +3,32 @@ const mongoose = require('mongoose');
 const moment = require('moment');
 const router = express.Router();
 const Listing = mongoose.model('Listing');
+const Site = mongoose.model('Site');
 
 /* GET listings main page. */
 router.get('/', function(req, res, next) {
-  Listing.find(function(err, listings) {
-    res.render('listings/index', {
-      title: 'Your Current Listings',
-      listings: listings
-    });
-  })
+  Listing
+    .find()
+    .populate('_site')
+    .exec(function(err, listings) {
+      console.log(listings);
+      res.render('listings/index', {
+        title: 'Your Current Listings',
+        listings: listings
+      });
+    })
 });
 
 
 // New listing form
 router.get('/new', function(req, res, next) {
-  res.render('listings/form', {
-    title: 'Create Listing',
-    listing: null
-  });
+  Site.find(function(err, sites) {
+    res.render('listings/form', {
+      title: 'Create Listing',
+      listing: null,
+      sites: sites
+    });
+  })
 });
 
 // Edit listing
@@ -45,17 +53,24 @@ router.get('/edit/:id', function(req, res, next) {
 
 // Create listing
 router.post('/create', function(req, res, next) {
-  req.body._site = '571bf12e06f0c9c109723160';
-  console.log(req.body);
+  Site.findById(req.body.site, function(err, site) {
 
-  listing = Listing.create(req.body, function(err, listing) {
-    if(err) {
-      console.log(err);
-      return next(err);
-    }
-    req.flash('success', 'New listing created');
-    res.redirect('/listings');
-  });
+    req.body._site = site._id;    // Connect the site to the listing.
+
+    listing = Listing.create(req.body, function(err, listing) {
+      if(err) {
+        console.log(err);
+        return next(err);
+      }
+      req.flash('success', 'New listing created');
+      res.redirect('/listings');
+    });
+
+
+
+  })
+
+
 
 });
 
